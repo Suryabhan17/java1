90fd6853367f9471cfabed90a4065df87b4e9713
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-26 10:13:44 +0530

    new file

--- add.java
+++ add.java
@@ -8,6 +8,9 @@ public class add{
 
   class add1 {
   public static void main(String args){
-     System.out.println("Suryabhan25");
+     System.out.println("Suryabhan3");
+     System.out.println("Suryabhan1");
+     System.out.println("Suryabhan26");
+     System.out.println("Suryabhan25_1");
   }
-}
+} 
