c04a93c82c34db5f7bc6ebb3f866e82339d396c8
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-26 10:56:02 +0530

    new

--- add2.java
+++ add2.java
@@ -1,13 +1,11 @@
 public class add2 {
-<<<<<<< HEAD
   public static void main (String args[]){
     System.out.println("2");
     
-  }
-=======
-  public static void main(String args[]){
+  
+
+  
     System.out.println("1");
   }
   
->>>>>>> new
 }
